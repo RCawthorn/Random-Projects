1e34b2d0a38e202a09ffc0c3915cc0ad7db50425
RCawthorn <R.Cawthorn@hotmail.co.uk>
2019-09-15 18:13:21 +0100

    First

---
+++ HighorLow/src/Main.java
@@ -0,0 +1,46 @@
+import javax.swing.*;
+import java.util.Scanner;
+import java.util.Random;
+public class Main {
+
+
+    public static void main(String[] args) {
+
+        Random random = new Random();
+
+        // Use Scanner for getting input from user
+
+        Scanner scanner = new Scanner(System.in);
+        String restart;
+        int number = random.nextInt(100);
+        int guess = -1;
+
+        while (guess != number) {
+            System.out.print("Enter your guess: ");
+
+            guess = scanner.nextInt();
+
+            if (guess < number) {
+
+                System.out.println("Too low, please try again");
+
+            } else if (guess > number) {
+
+                System.out.println("Too high, please try again");
+
+
+            } else if (guess == guess) {
+
+                // Guess is correct
+
+                System.out.println("Correct, the number was " + number);
+            }
+            // exit code
+            if (guess == 00) {
+                System.out.println("Exiting Game");
+
+                break;
+            }
+        }
+    }
+}
